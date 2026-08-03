2a41f1e8061cb2bccfc792d9e0b9ad860f2590d6
mosungjin <msj69076907@gmail.com>
2026-08-03 18:24:31 +0900

    [FIX] 외래키 추가

--- src/main/resources/db/migration/V3.1.1__add_related_action_id_to_meeting.sql
+++ src/main/resources/db/migration/V3.1.1__add_related_action_id_to_meeting.sql
@@ -1,7 +1,3 @@
 ALTER TABLE meeting
     ADD COLUMN related_action_id BIGINT NULL
-        COMMENT '이 회의가 참조하는 팀 액션 ID',
-    ADD CONSTRAINT fk_meeting_related_action
-        FOREIGN KEY (related_action_id)
-        REFERENCES action (id)
-        ON DELETE SET NULL;
+        COMMENT '이 회의가 참조하는 팀 액션 ID';
